e56efeedfaa836e67e30df5b379fffa1631ed070
GAtomis <850680822@qq.com>
2023-07-04 14:28:18 +0800

    rm: npm

--- node_modules/.pnpm/@tarojs+taro@3.6.8/node_modules/@tarojs/taro/index.js
+++
@@ -1,9 +0,0 @@
-const { hooks } = require('@tarojs/runtime')
-const taro = require('@tarojs/api').default
-
-if (hooks.isExist('initNativeApi')) {
-  hooks.call('initNativeApi', taro)
-}
-
-module.exports = taro
-module.exports.default = module.exports
